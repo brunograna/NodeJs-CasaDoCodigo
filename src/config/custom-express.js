e827f77d62e945e879dbded24752273a89672a37
Bruno Garcia Rana <bruno.rana@sensedia.local>
2019-09-30 12:07:08 -0300

    Course Part 2

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,22 +1,26 @@
-require("marko/node-require").install();
-require("marko/express");
-const bodyParser = require("body-parser");
+require('marko/node-require').install();
+require('marko/express');
 
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+app.use('/estatico', express.static('src/app/public'));
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+}));
 
-app.use("*", (req, resp, next)=>{
-    const inicio = new Date().getTime();
-    next();
-    const fim = new Date().getTime();
-    console.log(`This request took ${(fim-inicio)} ms`);
-});
-
-const routes = require("../app/routes/routes");
-routes(app);
+const rotas = require('../app/rotas/rotas');
+rotas(app);
 
 module.exports = app;
